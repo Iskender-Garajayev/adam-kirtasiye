a8f973e81390420773e519f317e1347e73c0d65f
Iskender Garajayev <yoloten.isa@gmail.com>
2023-10-13 20:40:23 +0300

    update Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import Header from './component/Header';
 
 function App() {
   return (
     <div className="App">
+      <h1>
+
       Merhaba Ismail ABI 
+      </h1>
+
+      <Header/>
 
       DAHA YENI KURULUMUNU YAPIYORUM
     </div>
---
+++ frontend/src/component/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Navbar, Nav, Container} from 'react-bootstrap'
+import {FaShoppingCart, FaUser} from 'react-icons/fa'
+
+const Header = () => {
+  return (
+    <header>
+
+      <FaShoppingCart/>
+      
+    </header>
+  )
+}
+
+export default Header
