aac72055f752cd2768129f7a6f3322d3052fcf1e
Iskender Garajayev <yoloten.isa@gmail.com>
2023-10-13 21:46:46 +0300

    update Header 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,7 @@ import Header from './component/Header';
 function App() {
   return (
     <div className="App">
-      <h1>
-
-      Merhaba Ismail ABI 
-      </h1>
-
       <Header/>
-
-      DAHA YENI KURULUMUNU YAPIYORUM
     </div>
   );
 }
--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -6,7 +6,23 @@ const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="#home">
+            ADA'M KIRTASIYE
+          </Navbar.Brand>
 
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+           <Nav className='ms-auto'>
+            <Nav.Link href='/cart' >
+              <FaShoppingCart/>
+            </Nav.Link>
+            <Nav.Link href='/login'>
+              <FaUser/>
+            </Nav.Link>
+           </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     
       
