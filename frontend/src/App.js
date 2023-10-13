53779a4102b951a601457c20c1627e23a7afe03e
Iskender Garajayev <yoloten.isa@gmail.com>
2023-10-13 20:21:33 +0300

    Q

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      hello 
-      
+      Merhaba Ismail ABI 
+
+      DAHA YENI KURULUMUNU YAPIYORUM
     </div>
   );
 }
