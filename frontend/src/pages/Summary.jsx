f2ed88d0125ffd76407297c5045f5062208e1e26
Iskender Garajayev <yoloten.isa@gmail.com>
2023-10-16 18:01:56 +0300

    update

--- frontend/src/pages/Summary.jsx
+++ frontend/src/pages/Summary.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Form, Button, Stack } from 'react-bootstrap'
+import Header from '../component/Header'
 
 const Summary = () => {
   return (
-    <Stack direction="horizontal" className="col-md-5 mx-auto" gap={3}>
+    <>
+    <Header/>
+
+    <Stack direction="horizontal" className="col-md-5 mx-auto py-6" gap={3} >
       <Form.Control className="me-auto" placeholder="Add your item here..." />
       <Button variant="secondary">Submit</Button>
     </Stack>
+    </>
   )
 }
 
