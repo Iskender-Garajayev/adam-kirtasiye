4ece9e8545b209a399acc36b7fcfd90db1dc292f
Iskender Garajayev <yoloten.isa@gmail.com>
2023-10-16 23:02:27 +0300

    update unversities component

--- frontend/src/pages/Books.jsx
+++ frontend/src/pages/Books.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import Header from '../component/Header'
-import { Button, Form, Stack } from 'react-bootstrap'
+import React from "react";
+import Header from "../component/Header";
+import { Button, Container, Form, Stack } from "react-bootstrap";
 
 const Books = () => {
   return (
     <>
-    <Header/>
+      <Header />
 
-    <Stack direction="horizontal" className="col-md-5 mx-auto py-6" gap={3} >
-      <Form.Control className="me-auto" placeholder="Kitap ismini secin..." />
-      <Button variant="success">Submit</Button>
-    </Stack>
+      <Container className="">
+        <Stack
+          direction="horizontal"
+          style={{ paddingTop: "15px" }}
+          className="col-md-5 mx-auto py-6"
+          gap={3}
+        >
+          <Form.Control
+            className="me-auto"
+            placeholder="Kitap ismini secin..."
+          />
+          <Button variant="success">Submit</Button>
+        </Stack>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
--- frontend/src/pages/Summary.jsx
+++ frontend/src/pages/Summary.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import { Form, Button, Stack } from 'react-bootstrap'
-import Header from '../component/Header'
+import React from "react";
+import { Form, Button, Stack, Container } from "react-bootstrap";
+import Header from "../component/Header";
 
 const Summary = () => {
   return (
     <>
-    <Header/>
+      <Header />
 
-    <Stack direction="horizontal" className="col-md-5 mx-auto py-6" gap={3} >
-      <Form.Control className="me-auto" placeholder="Kitap ozeti ismini secin... " />
-      <Button variant="success">Submit</Button>
-    </Stack>
+      <Container>
+        <Stack
+          direction="horizontal"
+          style={{ paddingTop: "15px" }}
+          className="col-md-5 mx-auto py-6"
+          gap={3}
+        >
+          <Form.Control
+            className="me-auto"
+            placeholder="Kitap ozeti ismini secin... "
+          />
+          <Button variant="success">Submit</Button>
+        </Stack>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default Summary
+export default Summary;
